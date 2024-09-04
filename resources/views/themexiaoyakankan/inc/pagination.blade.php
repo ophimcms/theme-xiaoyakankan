ef5855785b76823fdda189381d2b6fa0710a97bf
NTBDEV <32400888+binhnguyen1998822@users.noreply.github.com>
2024-09-04 22:12:28 +0700

    catalog

--- resources/views/themexiaoyakankan/inc/pagination.blade.php
+++ resources/views/themexiaoyakankan/inc/pagination.blade.php
@@ -1,30 +1,30 @@
-<div class="pagination-wrapper">
-    <ul class="pagination pagination-no-border">
-        <ul class="pagination">
-            @if ($paginator->hasPages())
-                @if ($paginator->onFirstPage())
-                @else
-                    <li><a class="" href="{{ $paginator->previousPageUrl() }}">←</a></li>
-                @endif
-                {{-- Pagination Elements --}}
-                @foreach ($elements as $element)
-                    {{-- Array Of Links --}}
-                    @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                            @if ($page == $paginator->currentPage())
-                                <li class="active"><a>{{ $page }}</a></li>
-                            @else
-                                <li><a href="{{ $url }}">{{ $page }}</a></li>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
-                {{-- Next Page Link --}}
-                @if ($paginator->hasMorePages())
-                    <li><a class="next page-numbers" href="{{ $paginator->nextPageUrl() }}">→</a></li>
-                @else
-                @endif
+<div class="gm-page">
+    <ul>
+        @if ($paginator->hasPages())
+            @if ($paginator->onFirstPage())
+            @else
+                <li class="first"><a href="{{ $paginator->previousPageUrl() }}">«</a></li>
             @endif
-        </ul>
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="pager on"><span>{{ $page }}</span></li>
+                        @else
+                            <li class="pager"><a href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="next"><a href="{{ $paginator->nextPageUrl() }}">»</a></li>
+            @else
+            @endif
+        @endif
+
+
     </ul>
 </div>
